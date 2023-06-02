f7342435e88c640f6f96aec687c3c789ce982c0c
Replit user <>
2023-06-02 02:56:44 +0000

    commit

--- myApp.js
+++ myApp.js
@@ -1,5 +1,29 @@
 let express = require('express');
 let app = express();
+const absolutePath = __dirname + '/views/index.html';
+const mySecret = process.env['MESSAGE_STYLE'];
+
+console.log("Hello World");
+
+app.get('/', (req, res) => {
+  res.sendFile(absolutePath)
+});
+
+// Middlewares
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/json", (req, res) => {
+  if (process.env.MESSAGE_STYLE === 'uppercase') {
+    res.json({
+      "message": "Hello json"
+    }.toUpperCase());
+  } else {
+    res.json({
+      "message": "Hello json"
+    });
+  }
+
+});
 
 
 
@@ -32,8 +56,4 @@ let app = express();
 
 
 
-
-
-
-
- module.exports = app;
+module.exports = app;
